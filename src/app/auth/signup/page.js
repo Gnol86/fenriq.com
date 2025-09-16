5ea93b4cd324714cd4e9d4f404ea8bb256c8478b
Arnaud M. <arnaud.marchot@icloud.com>
2025-09-16 14:51:08 +0200

    ♻️ refactor(page.js) : simplifier le rendu JSX en supprimant les fragments inutiles

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -9,18 +9,16 @@ import {
 
 export default function Page() {
     return (
-        <>
-            <Card className="w-sm">
-                <CardHeader>
-                    <CardTitle className="text-xl">S'inscrire</CardTitle>
-                    <CardDescription>
-                        Inscrivez-vous pour obtenir un accès à l'application
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <FormSignup />
-                </CardContent>
-            </Card>
-        </>
+        <Card className="w-sm">
+            <CardHeader>
+                <CardTitle className="text-xl">S'inscrire</CardTitle>
+                <CardDescription>
+                    Inscrivez-vous pour obtenir un accès à l'application
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <FormSignup />
+            </CardContent>
+        </Card>
     );
 }
