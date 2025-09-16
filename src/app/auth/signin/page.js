bd3c8cf193c21aa83eaf781778caa8f5fbd149d8
Arnaud M. <arnaud.marchot@icloud.com>
2025-09-16 02:32:49 +0200

    ✨ fonctionnalité(auth) : ajouter le formulaire de connexion et la page de connexion
    Cette mise à jour introduit un nouveau formulaire de connexion qui utilise
    la validation avec Zod et gère les erreurs de connexion. La page de
    connexion affiche le formulaire et fournit une interface utilisateur
    pour se connecter à l'application.

---
+++ src/app/auth/signin/page.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import FormSignin from "./form";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+export default function Page() {
+    return (
+        <Card className="w-sm">
+            <CardHeader>
+                <CardTitle className="text-xl">Se connecter</CardTitle>
+                <CardDescription>
+                    Connectez-vous à votre compte pour accéder à l'application
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <FormSignin />
+            </CardContent>
+        </Card>
+    );
+}
