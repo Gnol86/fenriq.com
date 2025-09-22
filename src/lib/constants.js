595717917617a9fcb16f3d6f3dc5f54fcde08785
Arnaud M. <arnaud.marchot@icloud.com>
2025-09-22 16:34:11 +0200

    Create constants.js

---
+++ src/lib/constants.js
@@ -0,0 +1,13 @@
+export const defaultRoleLabels = {
+    owner: "Propriétaire",
+    admin: "Admin",
+    member: "Membre",
+};
+
+export const invitationStatusLabels = {
+    accepted: "Acceptée",
+    pending: "En attente",
+    canceled: "Annulée",
+    rejected: "Refusée",
+    outdated: "Périmée",
+};
