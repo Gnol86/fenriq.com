af328259f9cad5f191cb4c2041439ccb7d5bb338
Arnaud M. <arnaud.marchot@icloud.com>
2025-10-08 02:13:37 +0200

    Simplify getServerUrl by removing unused checks

--- src/lib/server-url.js
+++ src/lib/server-url.js
@@ -1,18 +1,10 @@
 export const getServerUrl = () => {
-    if (typeof window !== "undefined") {
-        return window.location.origin;
-    }
-
-    if (process.env.PLAYWRIGHT_TEST_BASE_URL) {
-        return process.env.PLAYWRIGHT_TEST_BASE_URL;
-    }
-
-    if (process.env.VERCEL_ENV === "production") {
-        return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
-    }
-
     if (process.env.VERCEL_URL) {
         return `https://${process.env.VERCEL_URL}`;
+    }
+
+    if (process.env.BETTER_AUTH_BASE_URL) {
+        return process.env.BETTER_AUTH_BASE_URL;
     }
 
     return "http://localhost:3000";
