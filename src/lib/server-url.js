508ae7df4f562e455f25c9647f64529bf6131a39
Arnaud M. <arnaud.marchot@icloud.com>
2025-10-08 02:27:25 +0200

    Return window origin in getServerUrl on client side

--- src/lib/server-url.js
+++ src/lib/server-url.js
@@ -1,4 +1,8 @@
 export const getServerUrl = () => {
+    if (typeof window !== "undefined") {
+        return window.location.origin;
+    }
+
     if (process.env.VERCEL_URL) {
         return `https://${process.env.VERCEL_URL}`;
     }
