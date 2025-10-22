04f6380ddd1a86aa7463bee3fe4a37963be3b3db
Arnaud M. <arnaud.marchot@icloud.com>
2025-10-22 03:55:09 +0200

    Add support for custom AWS S3 endpoint configuration

--- src/lib/s3.js
+++ src/lib/s3.js
@@ -3,6 +3,8 @@ import { S3Client } from "@aws-sdk/client-s3";
 // Configure S3 client
 export const s3Client = new S3Client({
     region: process.env.AWS_S3_REGION,
+    endpoint: process.env.AWS_S3_HOST,
+    forcePathStyle: true, // Required for custom S3 endpoints
     credentials: {
         accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
