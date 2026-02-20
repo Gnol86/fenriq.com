a6a21f230b0f9b0482db99c330480580a911694d
Arnaud M. <arnaud.marchot@icloud.com>
2026-02-20 23:05:56 +0100

    Update dependencies, Prisma client, and config for latest versions

--- prisma.config.js
+++ prisma.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
     schema: path.join("prisma", "schema"),
+    datasource: {
+        url: process.env.DATABASE_URL,
+    },
 });
--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,23 +1,11 @@
-// src/lib/prisma-client.js
-import { PrismaClient } from "@root/prisma/generated";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@root/prisma/generated/client";
 
-// Instance singleton de PrismaClient avec configuration de logging
-// Cette instance est partagée dans toute l'application pour éviter les connexions multiples
 const globalForPrisma = globalThis;
 
-const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient({
-        // log:
-        //     process.env.NODE_ENV !== "production"
-        //         ? [
-        //               { emit: "stdout", level: "query" },
-        //               { emit: "stdout", level: "info" },
-        //               { emit: "stdout", level: "warn" },
-        //               { emit: "stdout", level: "error" },
-        //           ]
-        //         : [],
-    });
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
